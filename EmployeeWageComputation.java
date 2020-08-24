7fb51b602bbff1914d4a4fd8af2da573e6e676d2
Aparna <aparna61295@gmail.com>
2020-08-24 11:58:44 +0530

    Add:Program to calculate and display daiy and monthly wage until Max working days or Max hours reaches

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,40 +5,46 @@ public class EmployeeWageComputation{
    public static final int IS_FULL_TIME =2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NO_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_A_MONTH=100;
 
 	public static void main(String[] args){
 		System.out.println("****************Welcome to Employee WAge COmputation**************");
 
 		//VARIABLES DECLARATION
-		int empHrs= 0;
+		int dailyHrs= 0;
+		int monthlyHrs=0;
 		int dailyWage=0;
 		int monthlyWage=0;
-		int day;
+		int day=1;
 
-		//COMPUTE MONTHLY WAGE USING FOR LOOP
-		for(day=1; day<NO_OF_WORKING_DAYS; day++){
+		//COMPUTE MONTHLY WAGE UNTIL MAX HOURS(100) OR MAX NO OF WORKING DAYS(20) REACHES
 
+		while(monthlyHrs<=MAX_HRS_IN_A_MONTH && day < NO_OF_WORKING_DAYS){
+			day++;
 		//CHECK USER INPUT THROUGH RANDOM()
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
 		//CHECK WHETHER EMPLOYEE IS PRESENT FULL TIME/PART TIME OR ABSENT 
 			switch(empCheck){
 				case IS_FULL_TIME:
 					System.out.println("Employee is Present for Full Day");
-					empHrs=8;
+					dailyHrs=8;
 					break;
 
 				case IS_PART_TIME:
       			System.out.println("Employee is Present for Half Day");
-         		empHrs=4;
+         		dailyHrs=4;
 					break;
 
 				default:
 					System.out.println("Employee is Absent");
-					empHrs=0;
+					dailyHrs=0;
 			}
 
-		//COMPUTE DAILY WAGE OF AN EMPLOYEE USING CASE
- 			dailyWage=empHrs * EMP_RATE_PER_HOUR;
+			//CALCULATE WORKING HOURS
+			monthlyHrs+=dailyHrs;
+			System.out.println("Working Hours for Day"+day+": "+monthlyHrs);
+			//COMPUTE DAILY WAGE OF AN EMPLOYEE
+ 			dailyWage=dailyHrs * EMP_RATE_PER_HOUR;
 			monthlyWage+=dailyWage;
 			System.out.println("Daily Wage of an Employee for Day" +day+":"+dailyWage);
 		}
