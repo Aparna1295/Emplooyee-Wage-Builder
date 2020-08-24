613cd14589ce33c643dea308aa9174eb4d86ede1
Aparna <aparna61295@gmail.com>
2020-08-24 11:09:46 +0530

    Add:Program to calculate and display daily Wage of an Employee using Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,35 +1,39 @@
 public class EmployeeWageComputation{
+
+  //CONSTANTS DECLARATION
+	public static final int IS_PART_TIME =1;
+   public static final int IS_FULL_TIME =2;
+	public static final int EMP_RATE_PER_HOUR=20;
+
+
 	public static void main(String[] args){
 		System.out.println("****************Welcome to Employee WAge COmputation**************");
-		//CONSTANTS DECLARATION
-		int IS_PART_TIME =1;
-		int IS_FULL_TIME =2;
-
-		int EMP_RATE_PER_HOUR=20;
 
 		//VARIABLES DECLARATION
 		int empHrs= 0;
 		int empWage=0;
 
 		//CHECK USER INPUT THROUGH RANDOM()
-		double empCheck = Math.floor(Math.random()*10)%3;
-		//CHECK WHETHER EMPLOYEE IS PRESENT OR ABSENT
-		if (empCheck == IS_FULL_TIME){
+		int empCheck = (int)Math.floor(Math.random()*10)%3;
+		//CHECK WHETHER EMPLOYEE IS PRESENT FULL TIME/PART TIME OR ABSENT 
+		switch(empCheck){
+			case IS_FULL_TIME:
 			System.out.println("Employee is Present for Full Day");
 			empHrs=8;
-		}
-      else if(empCheck == IS_PART_TIME){
-			System.out.println("Employee is Present for Half Day");
+			break;
+			
+			case IS_PART_TIME:
+      	System.out.println("Employee is Present for Half Day");
          empHrs=4;
+			break;
 
-		}
-		else{
+			default:
 			System.out.println("Employee is Absent");
 			empHrs=0;
 		}
 
-		//COMPUTE DAILY WAGE OF AN EMPLOYEE
-		empWage=empHrs * EMP_RATE_PER_HOUR;
+		//COMPUTE DAILY WAGE OF AN EMPLOYEE USING CASE
+ 		empWage=empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Daily Wage of an Employee:"+empWage);
 	}
 
