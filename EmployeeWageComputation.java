b8437e6967e3b38605ceac6441e8dd13afdc6755
Aparna <aparna61295@gmail.com>
2020-08-24 10:52:42 +0530

    Add:Program to calculate and display daily Wage of a PartTime Employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,7 +2,9 @@ public class EmployeeWageComputation{
 	public static void main(String[] args){
 		System.out.println("****************Welcome to Employee WAge COmputation**************");
 		//CONSTANTS DECLARATION
-		int IS_FULL_TIME =1;
+		int IS_PART_TIME =1;
+		int IS_FULL_TIME =2;
+
 		int EMP_RATE_PER_HOUR=20;
 
 		//VARIABLES DECLARATION
@@ -10,16 +12,22 @@ public class EmployeeWageComputation{
 		int empWage=0;
 
 		//CHECK USER INPUT THROUGH RANDOM()
-		double empCheck = Math.floor(Math.random()*10)%2;
+		double empCheck = Math.floor(Math.random()*10)%3;
 		//CHECK WHETHER EMPLOYEE IS PRESENT OR ABSENT
 		if (empCheck == IS_FULL_TIME){
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present for Full Day");
 			empHrs=8;
+		}
+      else if(empCheck == IS_PART_TIME){
+			System.out.println("Employee is Present for Half Day");
+         empHrs=4;
+
 		}
 		else{
 			System.out.println("Employee is Absent");
 			empHrs=0;
 		}
+
 		//COMPUTE DAILY WAGE OF AN EMPLOYEE
 		empWage=empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Daily Wage of an Employee:"+empWage);
