a2ec372a60b61f8cbcb5a499f79015f999492678
Aparna <aparna61295@gmail.com>
2020-08-24 08:54:02 +0530

    Program to display whether Employee is Present or Absent

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,15 @@
 public class EmployeeWageComputation{
 	public static void main(String[] args){
 		System.out.println("****************Welcome to Employee WAge COmputation**************");
-
+		//CONSTANTS DECLARATION
+		int IS_FULL_TIME =1;
+		//CHECK USER INPUT THROUGH RANDOM()
+		double empCheck = Math.floor(Math.random()*10)%2;
+		//CHECK WHETHER EMPLOYEE IS PRESENT OR ABSENT
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 
 	}
 
