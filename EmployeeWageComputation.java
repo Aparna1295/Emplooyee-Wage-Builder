e7e55afe1bc9aa9864d7f7c88ebfe5d69cd9d296
Aparna <aparna61295@gmail.com>
2020-08-26 07:54:58 +0530

    Add:Program to calculate and display daily & monthly wage of an Emplo…

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,3 +1,37 @@
+class CompanyWage{
+
+ // CLASS VARIABLES
+   protected final String company;
+   protected final int noOfDaysPerMonth;
+   protected final int maxHrsPerMonth;
+   protected final int ratePerHr;
+   private int monthlyWage;
+
+	public CompanyWage(String company,int ratePerHr, int noOfDaysPerMonth, int maxHrsPerMonth){
+
+		this.company = company;
+		this.ratePerHr = ratePerHr;
+		this.noOfDaysPerMonth = noOfDaysPerMonth;
+		this.maxHrsPerMonth = maxHrsPerMonth;
+
+	}
+
+	public void setMonthlyWage(int monthlyWage){
+
+		this.monthlyWage=monthlyWage;
+
+	}
+
+	@Override
+	public String toString(){
+
+	   return "Monthly Wage of an Employee of "+company+": " +monthlyWage;
+
+	}
+
+}
+
+
 public class EmployeeWageComputation{
 
   //CONSTANTS DECLARATION
@@ -5,33 +39,46 @@ public class EmployeeWageComputation{
    public static final int IS_FULL_TIME =2;
 
 	//	CLASS VARIABLES
-	private final String company;
-   private final int ratePerHr;
-   private final int noOfDaysPerMonth;
-   private final int maxHrsPerMonth;
-	private int monthlyWage;
+	private int numOfCompany=0;
+	private CompanyWage[] companyWageArray;
 
 	//PARAMETERIZED CONSTRUCTOR
-	public EmployeeWageComputation(String company, int ratePerHr, int noOfDaysPerMonth, int maxHrsPerMonth){
+	public EmployeeWageComputation(){
 
-		this.company=company;
-		this.ratePerHr=ratePerHr;
-		this.noOfDaysPerMonth=noOfDaysPerMonth;
-		this.maxHrsPerMonth=maxHrsPerMonth;
-
+	companyWageArray= new CompanyWage[5];
 
 	}
 
-	public void computeWage(){
+
+	private void addCompanyWage(String company,int ratePerHr, int noOfDaysPerMonth, int maxHrsPerMonth){
+
+		companyWageArray[numOfCompany]=new CompanyWage(company,ratePerHr,noOfDaysPerMonth,maxHrsPerMonth);
+		numOfCompany++;
+
+	}
+
+
+	private void computeWage(){
+
+		for(int i=0; i<numOfCompany; i++){
+
+			companyWageArray[i].setMonthlyWage(this.computeWage(companyWageArray[i]));
+			System.out.println(companyWageArray[i]);
+
+		}
+
+	}
+
+	public int computeWage(CompanyWage companyWage){
 		//VARIABLES DECLARATION
 		int dailyHrs= 0;
 		int monthlyHrs=0;
 		int dailyWage=0;
 		int day=1;
-
+		
 		//COMPUTE MONTHLY WAGE UNTIL MAX HOURS(100) OR MAX NO OF WORKING DAYS(20) REACHES
-		System.out.println("-----------Employee Wage Computation for "+company+"------------");
-		while(monthlyHrs<=maxHrsPerMonth && day < noOfDaysPerMonth){
+		while(monthlyHrs<=companyWage.maxHrsPerMonth && day < companyWage.noOfDaysPerMonth){
+			System.out.println("------------Monthly Wage Computation for EMployee of  " +companyWage.company);
 			day++;
 		//CHECK USER INPUT THROUGH RANDOM()
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
@@ -56,32 +103,21 @@ public class EmployeeWageComputation{
 			monthlyHrs+=dailyHrs;
 			System.out.println("Working Hours for Day"+day+": "+monthlyHrs);
 			//COMPUTE DAILY WAGE OF AN EMPLOYEE
- 			dailyWage=dailyHrs * ratePerHr;
-			monthlyWage+=dailyWage;
+ 			dailyWage=dailyHrs * companyWage.ratePerHr;
 			System.out.println("Daily Wage of an Employee for Day" +day+":"+dailyWage);
 		}
-      System.out.println("An Employee worked for "+monthlyHrs+" Hours and "+day+" Days");
+      return  monthlyHrs * companyWage.ratePerHr;
 
 	}
 
-	@Override
-	public String toString(){
-
-		return "Monthly Wage of an Employee for "+company+": " +monthlyWage;
-
-
-	}
 	public static void main(String[] args){
 		System.out.println("****************Welcome to Employee Wage Computation**************");
-		
-		//CREATE OBJECTS  TO INVOKE CLASS METHODS
-		EmployeeWage accenture = new EmployeeWage("Accenture",15,20,84);
-		EmployeeWage citicorp = new EmployeeWage("Citi Corp",20,22,98);
-		accenture.computeWage();
-		System.out.println(accenture);
-      citicorp.computeWage();
-      System.out.println(citicorp);
 
+		//CREATE OBJECTS  TO INVOKE CLASS METHODS
+		EmployeeWageComputation empWageCompute= new EmployeeWageComputation();
+		empWageCompute.addCompanyWage("Accenture",15,20,80);
+		empWageCompute.addCompanyWage("Citi Corp",20,25,90);
+      empWageCompute.computeWage();
 
 	}
 
